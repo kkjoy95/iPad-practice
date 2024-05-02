95750b353a1d1a8136da48843e24dd451b719fce
kkjoy95 <dmswl9637@gamil.com>
2024-05-02 17:50:02 +0900

    아이패드 홈페이지 상단 메뉴 - 검색바

--- js/main.js
+++ js/main.js
@@ -24,3 +24,28 @@ function showBasket(){
 function hideBasket(){
     basketEl.classList.remove('show')
 }
+
+
+// 검색창
+const headerEl = document.querySelector('header')
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+const searchShadowEl = searchWrapEl.querySelector('.shadow')
+
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch)
+
+
+function showSearch(){
+headerEl.classList.add('searching')
+document.documentElement.classList.add('fixed')
+}
+function hideSearch(){
+headerEl.classList.remove('searching')
+document.documentElement.classList.remove('fixed')
+
+}
+
