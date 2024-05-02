2b8ced161cc852fe3eaccda6cd0f3f1b46dc0ec2
kkjoy95 <dmswl9637@gamil.com>
2024-05-02 16:32:14 +0900

    프로젝트 생성

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// 장바구니 드롭 다운 메뉴
+const basketStarterEl = document.querySelector('header .basket-starter')
+const basketEl = basketStarterEl.querySelector('.basket')
+
+basketStarterEl.addEventListener('click', function(event){
+    event.stopPropagation()
+    if(basketEl.classList.contains('show')){
+        hideBasket()
+    }else{
+        showBasket()
+    }
+})
+basketEl.addEventListener('click', function(event){
+    event.stopPropagation()
+})
+
+window.addEventListener('click', function(){
+    hideBasket()
+})
+
+function showBasket(){
+    basketEl.classList.add('show')
+}
+function hideBasket(){
+    basketEl.classList.remove('show')
+}
