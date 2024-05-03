553b8061620949822a4974ead890ea822cb6210c
kkjoy95 <dmswl9637@gamil.com>
2024-05-03 17:44:10 +0900

    Power-MultipleApps 스타일 생성

--- js/main.js
+++ js/main.js
@@ -2,36 +2,39 @@
 const basketStarterEl = document.querySelector('header .basket-starter')
 const basketEl = basketStarterEl.querySelector('.basket')
 
-basketStarterEl.addEventListener('click', function(event){
+basketStarterEl.addEventListener('click', function (event) {
     event.stopPropagation()
-    if(basketEl.classList.contains('show')){
+    if (basketEl.classList.contains('show')) {
         hideBasket()
-    }else{
+    } else {
         showBasket()
     }
 })
-basketEl.addEventListener('click', function(event){
+basketEl.addEventListener('click', function (event) {
     event.stopPropagation()
 })
 
-window.addEventListener('click', function(){
+window.addEventListener('click', function () {
     hideBasket()
 })
 
-function showBasket(){
+function showBasket() {
     basketEl.classList.add('show')
 }
-function hideBasket(){
+function hideBasket() {
     basketEl.classList.remove('show')
 }
 
 
 // 검색창
 const headerEl = document.querySelector('header')
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]
 const searchWrapEl = headerEl.querySelector('.search-wrap')
 const searchStarterEl = headerEl.querySelector('.search-starter')
 const searchCloserEl = searchWrapEl.querySelector('.search-closer')
 const searchShadowEl = searchWrapEl.querySelector('.shadow')
+const searchInputEl = searchWrapEl.querySelector('input')
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
 
 
 searchStarterEl.addEventListener('click', showSearch)
@@ -39,13 +42,30 @@ searchCloserEl.addEventListener('click', hideSearch)
 searchShadowEl.addEventListener('click', hideSearch)
 
 
-function showSearch(){
-headerEl.classList.add('searching')
-document.documentElement.classList.add('fixed')
+function showSearch() {
+    headerEl.classList.add('searching')
+    document.documentElement.classList.add('fixed')
+    headerMenuEls.reverse().forEach(function (el, index) {
+        el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+    })
+    searchDelayEls.forEach(function (el, index) {
+        el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+    })
+    setTimeout(function () {
+        searchInputEl.focus()
+    }, 600)
 }
-function hideSearch(){
-headerEl.classList.remove('searching')
-document.documentElement.classList.remove('fixed')
+function hideSearch() {
+    headerEl.classList.remove('searching')
+    document.documentElement.classList.remove('fixed')
+    headerMenuEls.reverse().forEach(function (el, index) {
+        el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+    })
+    searchDelayEls.reverse().forEach(function (el, index) {
+        el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+    })
+    searchDelayEls.reverse()
+    searchInputEl.value = ''
+}
 
-}
 
