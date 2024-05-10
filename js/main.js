cc13be821e1b2b072fa8d808e29e82a49e6f07a6
kkjoy95 <dmswl9637@gamil.com>
2024-05-10 17:23:59 +0900

    헤더 반응형 제작

--- js/main.js
+++ js/main.js
@@ -41,13 +41,16 @@ const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
 
 
 searchStarterEl.addEventListener('click', showSearch)
-searchCloserEl.addEventListener('click', hideSearch)
+searchCloserEl.addEventListener('click', function(event){
+    event.stopPropagation()
+    hideSearch()
+})
 searchShadowEl.addEventListener('click', hideSearch)
 
 
 function showSearch() {
     headerEl.classList.add('searching')
-    document.documentElement.classList.add('fixed')
+    stopScroll()
     headerMenuEls.reverse().forEach(function (el, index) {
         el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
     })
@@ -60,7 +63,7 @@ function showSearch() {
 }
 function hideSearch() {
     headerEl.classList.remove('searching')
-    document.documentElement.classList.remove('fixed')
+    playScroll()
     headerMenuEls.reverse().forEach(function (el, index) {
         el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
     })
@@ -70,8 +73,78 @@ function hideSearch() {
     searchDelayEls.reverse()
     searchInputEl.value = ''
 }
+function playScroll(){
+    document.documentElement.classList.remove('fixed')
+}
+function stopScroll(){
+    document.documentElement.classList.add('fixed')
+}
 
 
+
+// 헤더메뉴 토글!
+const menuStarterEl = document.querySelector('header .menu-starter')
+menuStarterEl.addEventListener('click', function(){
+   if(headerEl.classList.contains('menuing')){
+    headerEl.classList.remove('menuing')
+    searchInputEl.value = ''
+    playScroll()
+   }else{
+    headerEl.classList.add('menuing')
+    stopScroll()
+   } 
+})
+
+// 헤더검색!
+const searchTextFieldEl = document.querySelector('header .textfield')
+const searchCancelEl = document.querySelector('header .search-canceler')
+searchTextFieldEl.addEventListener('click', function(){
+    headerEl.classList.add('searching--mobile')
+    searchInputEl.focus()
+})
+searchCancelEl.addEventListener('click',function(){
+    headerEl.classList.remove('searching--mobile')
+})
+
+
+// 모바일과 데스크탑 모드의 검색창 스타일이 겹치지 않게 하기
+window.addEventListener('resize', function(){
+    if(window.innerWidth <= 740){
+        headerEl.classList.remove('searching')
+    }else{
+        headerEl.classList.remove('searching--mobile')
+    }
+})
+
+
+// 
+const navEl = document.querySelector('nav')
+const navMenuToggleEl = navEl.querySelector('.menu-toggler')
+const navMenuShadowEl = navEl.querySelector('.shadow')
+
+navMenuToggleEl.addEventListener('click', function(){
+   if(navEl.classList.contains('menuing')){
+    hideNavMenu()
+   } else{
+    showNavMenu()
+   }
+})
+navEl.addEventListener('click', function(event){
+    event.stopPropagation()
+})
+navMenuShadowEl.addEventListener('click', hideNavMenu)
+window.addEventListener('click', hideNavMenu)
+function showNavMenu(){
+    navEl.classList.add('menuing')
+}
+function hideNavMenu(){
+    navEl.classList.remove('menuing')
+}
+
+
+
+
+// 요소의 가시성 관찰
 const io = new IntersectionObserver(entries => {
     // entries는 `io.observe(el)`로 등록된 모든 관찰 대상 배열.
     entries.forEach(entry => {
