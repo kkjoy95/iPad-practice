5dc092f4b6f46ddac9a8b207b15b56388e44d4f7
kkjoy95 <dmswl9637@gamil.com>
2024-05-13 13:43:50 +0900

    반응형 레이아웃 작업

--- js/main.js
+++ js/main.js
@@ -231,6 +231,7 @@ navigations.forEach(function(nav){
     mapEl.innerHTML = /* html */`
     <h3>
         <span class="text">${nav.title}</span>
+        <span class="icon">+</span>
     </h3>
     <ul>
         ${mapList}
@@ -244,3 +245,12 @@ navigations.forEach(function(nav){
 // Footer / Copyright / this-year
 const thisYearEl = document.querySelector('span.this-year')
 thisYearEl.textContent = new Date().getFullYear()
+
+// Footer / Navigation mobile
+const mapEls = document.querySelectorAll('footer .navigations .map')
+mapEls.forEach(function(el){
+    const h3El = el.querySelector('h3')
+    h3El.addEventListener('click', function(){
+        el.classList.toggle('active')
+    })
+})
